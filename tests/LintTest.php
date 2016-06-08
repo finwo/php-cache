438e3eb7b32554db89694c7f8e594ef42fa1f4ce
Robin Bron <robin@finwo.nl>
2016-06-08 10:51:09 +0200

    Added clear comments on the LintTest

--- tests/LintTest.php
+++ tests/LintTest.php
@@ -4,18 +4,21 @@ class LintTest extends \PHPUnit_Framework_TestCase
 {
     public function testSrc()
     {
+        // Main entry
         $fileList = glob('src/');
 
-        $i = 0;
-
+        // Loop through known files
         while ( $inode = array_shift($fileList) ) {
 
+            // Try to iterate deeper
             if ( is_dir($inode) ) {
                 $fileList = array_merge($fileList, glob(realpath($inode).'/*'));
                 continue;
             }
 
-            if (preg_match('/^.+\.php$/i', $inode)) {
+            // If we're a PHP file
+            if (preg_match('/^.+\.(php|inc)$/i', $inode)) {
+                // Run a unit test
                 $this->lintFile($inode);
             }
 
@@ -24,7 +27,10 @@ class LintTest extends \PHPUnit_Framework_TestCase
 
     private function lintFile($filename = '')
     {
+        // Show progress in the terminal
         print('.');
+
+        // And actually run the test (with proper error message)
         $this->assertContains('No syntax errors', exec(sprintf('php -l "%s"', $filename), $out), sprintf("%s contains syntax errors", $filename));
     }
 }
